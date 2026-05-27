2c7bbf3b49e68f970bb6d51b75e11e73e5e9cde4
2025050210 <yesjs120716@gmail.com>
2026-05-27 15:15:07 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,28 +3,24 @@
 int main(void)
 {
     char arr[11];
-    char max_char;
+    char max_char='\0';
     int max_count=0;
-    int current_count=0;
-    char *p;
-    char *q;
     
     scanf("%10c", arr);
     
     for(char *p=arr; p<arr+10; p++){
-        current_count = 0;
+        int current_count = 0;
         for(char *q=arr; q<arr+10; q++){
             if(*p==*q)
             current_count++;
         }
         if(current_count>max_count){
         max_count=current_count;
-        max_count=*p;
+        max_char=*p;
+        }
     }
     
-    }
     printf("%c %d\n", max_char, max_count);
     
-
     return 0;
 }
